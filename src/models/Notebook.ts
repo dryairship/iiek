e2aa26e52164afc4945caed72e65fb5033ead076
Priydarshi Singh <dryairship@gmail.com>
2021-01-08 17:58:38 +0530

    Add Notebook logic

--- src/models/Notebook.ts
+++ src/models/Notebook.ts
@@ -1,3 +1,73 @@
+enum ValueType {
+    NUMBER, STRING
+}
+
+class Value {
+    type: ValueType;
+    data: number | string;
+
+    constructor(type: ValueType, data: number | string) {
+        this.type = type;
+        this.data = data;
+    }
+}
+
 class Notebook {
-    
+    values: Value[];
+
+    count(): number {
+        return this.values.length;
+    }
+
+    clear(): void {
+        this.values = [];
+    }
+
+    pushNumber(value: number): void {
+        this.values.push(new Value(ValueType.NUMBER, value));
+    }
+
+    pushString(value: string): void {
+        this.values.push(new Value(ValueType.STRING, value));
+    }
+
+    assertNumbers(): boolean {
+        return this.values.every(
+            value => value.type = ValueType.NUMBER
+        );
+    }
+
+    assertStrings(): boolean {
+        return this.values.every(
+            value => value.type = ValueType.STRING
+        );
+    }
+
+    popAllNumbers(): number[] {
+        let result = this.values.map(
+            value => {
+                if (typeof value.data === 'number') {
+                    return value.data;
+                } else {
+                    return 0;
+                }
+            }
+        );
+        this.values = [];
+        return result;
+    }
+
+    popAllStrings(): string[] {
+        let result = this.values.map(
+            value => {
+                if (typeof value.data === 'string') {
+                    return value.data;
+                } else {
+                    return "";
+                }
+            }
+        );
+        this.values = [];
+        return result;
+    }
 }
